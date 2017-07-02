e2c8651f3d6dd00960b877ab5b05a8d745e3fa48
Guillaume Lemaitre <g.lemaitre58@gmail.com>
2017-07-02 23:49:35 +0200

    Move constant variable in setup.py

--- Day_2_Software_engineering_best_practices/solutions/08_github_travis/spectra_analysis/version.py
+++ Day_2_Software_engineering_best_practices/solutions/08_github_travis/spectra_analysis/version.py
@@ -16,39 +16,3 @@ if _version_extra:
     _ver.append(_version_extra)
 
 __version__ = '.'.join(map(str, _ver))
-
-CLASSIFIERS = ["Development Status :: 3 - Alpha",
-               "Environment :: Console",
-               "Intended Audience :: Science/Research",
-               "License :: OSI Approved :: MIT License",
-               "Operating System :: OS Independent",
-               "Programming Language :: Python",
-               "Topic :: Scientific/Engineering"]
-
-# Description should be a one-liner:
-description = "Spectra Analysis: a CDS template for python package"
-# Long description will go up on the pypi page
-long_description = """
-
-Spectra Analysis
-================
-Is is a toy example to learn how to make a python package.
-"""
-
-NAME = "spectra_analysis"
-MAINTAINER = "Center for Data Science"
-MAINTAINER_EMAIL = "cdsupsay@gmail.com"
-DESCRIPTION = description
-LONG_DESCRIPTION = long_description
-URL = "https://github.com/paris-saclay-cds/python-workshop"
-DOWNLOAD_URL = ""
-LICENSE = "MIT"
-AUTHOR = "Center for Data Science"
-AUTHOR_EMAIL = "cdsupsay@gmail.com"
-PLATFORMS = "OS Independent"
-MAJOR = _version_major
-MINOR = _version_minor
-MICRO = _version_micro
-VERSION = __version__
-PACKAGE_DATA = {'spectra_analysis': [pjoin('tests', 'data', '*')]}
-REQUIRES = ["numpy", "pandas", "scipy", 'scikit-learn', "matplotlib", "six"]
